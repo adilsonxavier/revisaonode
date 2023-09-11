059fdc280420f1e46a3176d4639eebeeff111834
adilsonxavier <adilsonxdesouza@gmail.com>
2023-09-11 10:58:38 -0300

    alteração teste develop

--- backend/src/middleware/tasksMiddleware.js
+++ backend/src/middleware/tasksMiddleware.js
@@ -6,7 +6,7 @@ const validateBody = (req, res,next) => {
     if (body.title === "") {
         return res.status(400).json({ message: "O campo 'title' não pode ser vazio" });
     }
-
+   var a = 1; // at=lteração teste
     next();
     // A função callback next() é requisito do próprio router do express para que seja executada a próxima método
     //  no router caso passe pelos ifs da validação
@@ -14,6 +14,7 @@ const validateBody = (req, res,next) => {
     // router.post("/tasks",tasksMiddleware.validateBody, taskController.createTask)
     // Se passar pela validação em tasksMiddleware.validateBody ai vai pro próximo método taskController.createTask
     // funciona també se o nome da função não for exatamente "next()"
+
 };
 
 
